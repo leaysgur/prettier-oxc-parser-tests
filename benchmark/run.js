c425ef35670fef690910f43baa7cff70b47e7430
Yuji Sugiura <y.sugiura.0316@gmail.com>
2025-05-07 17:23:15 +0900

    Update bench structure

--- benchmark/run.js
+++ benchmark/run.js
@@ -3,16 +3,17 @@ import { bench, run } from "mitata";
 import * as prettier from "prettier";
 import pkg from "../package.json" with { type: "json" };
 
-const FIXTURES = await readdir("./benchmark/fixtures");
+const JS_FIXTURES_DIR = "./benchmark/fixtures/js";
+const TS_FIXTURES_DIR = "./benchmark/fixtures/ts";
 
 console.log("- OXC version:", "`" + pkg.dependencies["oxc-parser"] + "`");
 console.log("- Prettier version:", "`" + pkg.devDependencies.prettier + "`");
 console.log("");
 
 console.log("### JS(X)");
-for (const file of FIXTURES) {
+for (const file of await readdir(JS_FIXTURES_DIR)) {
   console.log("#### %s", file);
-  const CODE = await readFile(`./benchmark/fixtures/${file}`, "utf8");
+  const CODE = await readFile(`${JS_FIXTURES_DIR}/${file}`, "utf8");
 
   bench("$parser", function* (state) {
     const parser = state.get("parser");
@@ -24,6 +25,28 @@ for (const file of FIXTURES) {
 
     yield async () => await prettier.format(CODE, options);
   }).args("parser", ["oxc", "babel"]);
+
+  await run({ format: "markdown" });
+  console.log("");
+}
+
+console.log("");
+
+console.log("### TS(X)");
+for (const file of await readdir(TS_FIXTURES_DIR)) {
+  console.log("#### %s", file);
+  const CODE = await readFile(`${TS_FIXTURES_DIR}/${file}`, "utf8");
+
+  bench("$parser", function* (state) {
+    const parser = state.get("parser");
+
+    const options = {
+      parser,
+      plugins: parser.startsWith("oxc") ? ["./src/index.js"] : [],
+    };
+
+    yield async () => await prettier.format(CODE, options);
+  }).args("parser", ["oxc-ts", "typescript"]);
 
   await run({ format: "markdown" });
   console.log("");
