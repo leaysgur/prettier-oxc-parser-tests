5d16c26736d136291712b763195e06dad2c7509d
Yuji Sugiura <y.sugiura.0316@gmail.com>
2025-05-08 12:30:50 +0900

    Fix

--- debug.js
+++ debug.js
@@ -2,10 +2,9 @@ import * as prettier from "prettier";
 
 // DEBUG: Read from file
 // import { readFileSync} from "node:fs";
-// const CODE = readFileSync("./benchmark/fixtures/3930-kb.js", "utf-8");
+// const CODE = readFileSync("./benchmark/fixtures/ts/2922-kb.ts", "utf-8");
 const CODE = `
-// aooo
-let a,b=1,c=[2,3,,4,]
+enum A { a, b }
 `.trim();
 const IS_JS = false;
 
